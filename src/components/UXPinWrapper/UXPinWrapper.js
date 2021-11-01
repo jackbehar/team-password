1853c7ca31040e9bb4294743d1ca2743e4ad7b05
Jack Behar <jack@uxpin.com>
2021-11-01 14:15:31 -0600

    Theming

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import boilerplateTheme from "./boilerplate-theme";
+import teamPasswordTheme from "./team-password-theme";
 
-const theme = createTheme(boilerplateTheme);
+const theme = createTheme(teamPasswordTheme);
 
 export default function UXPinWrapper({ children }) {
-
+  if (!document.getElementById("mui-merge-font")) {
+    let muli = document.createElement("link");
+    muli.setAttribute(
+      "href",
+      "https://fonts.googleapis.com/css?family=Muli:400,500,300,700"
+    );
+    muli.setAttribute("rel", "stylesheet");
+    muli.setAttribute("id", "mui-merge-font");
+    document.head.appendChild(muli);
+  }
   return (<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 }
---
+++ src/components/UXPinWrapper/team-password-theme.js
@@ -0,0 +1,29 @@
+const boilerplateTheme = {
+  // To further customize this theme,
+  // please see the full theme object here: https://mui.com/customization/default-theme/
+  palette: {
+    primary: {
+      main: "#1e4d6a",
+      light: "#63dae5",
+      dark: "#10313e",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      main: "#63dae5",
+      // light: "#5ebf9926",
+      // dark: "#47b586",
+      contrastText: "#ffffff"
+    },
+    text:{
+      primary: "#1e4d6a",
+      secondary: "#63dae5"
+    },
+  },
+  typography: {
+    fontFamily: 'Muli, sans-serif',
+    // fontSize: 16,
+  },
+};
+
+export default boilerplateTheme;
+
