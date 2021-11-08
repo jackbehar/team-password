7ed17d903d92e08fed255f9f66945c1702da7eea
Jack Behar <jack@uxpin.com>
2021-11-08 16:52:02 -0700

    Theming updates

--- src/components/UXPinWrapper/team-password-theme.js
+++ src/components/UXPinWrapper/team-password-theme.js
@@ -1,3 +1,5 @@
+import { lineHeight } from "@mui/system";
+
 const boilerplateTheme = {
   // To further customize this theme,
   // please see the full theme object here: https://mui.com/customization/default-theme/
@@ -10,11 +12,11 @@ const boilerplateTheme = {
     },
     secondary: {
       main: "#63dae5",
-      light: "#5ebf9926",
+      light: "#f2f5f9",
       dark: "#47b586",
       contrastText: "#ffffff"
     },
-    text:{
+    text: {
       primary: "#1e4d6a",
       secondary: "#63dae5"
     },
@@ -22,6 +24,11 @@ const boilerplateTheme = {
   typography: {
     fontFamily: 'Muli, sans-serif',
     fontSize: 15,
+    h2: {
+      fontWeight: "700",
+      fontSize: "44px",
+      lineHeight: "56px"
+    },
     button: {
       textTransform: "capitalize",
       fontWeight: "700",
@@ -35,11 +42,25 @@ const boilerplateTheme = {
         root: {
           borderRadius: 6,
         },
-        text:{
+        text: {
           fontWeight: "500",
         }
       },
     },
+    MuiToolbar: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          borderRadius: 6,
+          "& .MuiButton-text:hover": { "background-color": "#ffffff20" }
+        },
+      },
+    },
+    // MuiIcon:{
+    //   root:{
+    //     fontFamily: 'Material Icons Outlined'
+    //   },
+    // },
   },
 };
 
