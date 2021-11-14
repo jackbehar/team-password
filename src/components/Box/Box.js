5511c2e14f528669c27ea17d061e5ac3d1529772
Jack Behar <jack@uxpin.com>
2021-11-14 12:17:35 -0700

    testing payload issue

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -129,46 +129,46 @@ Box.propTypes = {
     "right"
   ]),
 
-  textDecoration: PropTypes.oneOf([
-    "underline",
-    "overline",
-    "line-through"
-  ]),
+  // textDecoration: PropTypes.oneOf([
+  //   "underline",
+  //   "overline",
+  //   "line-through"
+  // ]),
 
-  display: PropTypes.oneOf(['block', 'inline', 'inline-block', 'flex', 'inline-flex', 'grid', 'inline-grid', 'flow-root']),
+  // display: PropTypes.oneOf(['block', 'inline', 'inline-block', 'flex', 'inline-flex', 'grid', 'inline-grid', 'flow-root']),
   
-  justifyContent: PropTypes.oneOf([
-    "flex-start",
-    "center",
-    "flex-end",
-    "space-between",
-    "space-around",
-    "space-evenly"
-  ]),
+  // justifyContent: PropTypes.oneOf([
+  //   "flex-start",
+  //   "center",
+  //   "flex-end",
+  //   "space-between",
+  //   "space-around",
+  //   "space-evenly"
+  // ]),
 
-  alignContent: PropTypes.oneOf([
-    "stretch",
-    "center",
-    "flex-start",
-    "flex-end",
-    "space-between",
-    "space-around"
-  ]),
+  // alignContent: PropTypes.oneOf([
+  //   "stretch",
+  //   "center",
+  //   "flex-start",
+  //   "flex-end",
+  //   "space-between",
+  //   "space-around"
+  // ]),
 
-  alignItems: PropTypes.oneOf([
-    "flex-start",
-    "center",
-    "flex-end",
-    "stretch",
-    "baseline"
-  ]),
+  // alignItems: PropTypes.oneOf([
+  //   "flex-start",
+  //   "center",
+  //   "flex-end",
+  //   "stretch",
+  //   "baseline"
+  // ]),
 
-  alignSelf: PropTypes.oneOf([
-    "stretch",
-    "center",
-    "start",
-    "end",
-  ]),
+  // alignSelf: PropTypes.oneOf([
+  //   "stretch",
+  //   "center",
+  //   "start",
+  //   "end",
+  // ]),
 
   // /** @uxpinignoreprop */
   // flex: PropTypes.string,
