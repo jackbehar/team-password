9a3f1554c257ca0c85b240c8e39865e391468544
Jack Behar <jack@uxpin.com>
2021-11-16 12:06:24 -0700

    Theming

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -154,7 +154,8 @@ Grid.propTypes = {
     paddingRight: PropTypes.string,
   
     paddingBottom: PropTypes.string,
-  
+    
+    paddingLeft: PropTypes.string,
     /**
      * The system prop that allows defining system overrides as well as additional CSS styles. 
      * See the `sx` page for more details https://mui.com/system/the-sx-prop/.
